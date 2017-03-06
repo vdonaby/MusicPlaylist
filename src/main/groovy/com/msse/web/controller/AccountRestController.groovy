0b51735e7ece6d5bbb446ed10d4bb558577681b2
Shorouq <shorouq.a.1@gmail.com>
2017-03-05 20:38:52 -0600

    1- AccountRestController 2- AccountTests    2-A AccountFunctionalTests    2-B AccountIntegrationTests

---
+++ src/main/groovy/com/msse/web/controller/AccountRestController.groovy
@@ -0,0 +1,15 @@
+package com.msse.web.controller
+
+/**
+ A1: Receives JSON data to create an Account
+ A2: Return an error response from the create Account endpoint if the account values are invalid
+ A3: Returns JSON data with Account values for a user based on an account id or email. (data-driven test)
+ A4: Returns the playlists for an Account and is sortable and pageable
+ A5: All steps must have valid functional tests
+ A6: Create integration tests for Account
+ */
+
+
+class AccountRestController {
+
+}
---
+++ src/test/groovy/com/msse/web/AccountTest/AccountIntegrationTests.groovy
@@ -0,0 +1,8 @@
+package com.msse.web.AccountTest
+
+/**
+ A6: Create integration tests for Account
+ */
+
+class AccountIntegrationTests {
+}
