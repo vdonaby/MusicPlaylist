02b87f1a519e34a9869d0fd63727bea1085b9cc6
Shorouq <aljoh009@umn.edu>
2017-04-22 02:25:23 -0500

    testing component: yes (commented .toBeDefined method) testing data binding: no

--- src/main/javascript/app/src/app/component/artist/artist.component.spec.ts
+++ src/main/javascript/app/src/app/component/artist/artist.component.spec.ts
@@ -1,25 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import { ArtistComponent } from './artist.component';
 
 describe('ArtistComponent', () => {
   let component: ArtistComponent;
+  //the result to provide feature for testing component
   let fixture: ComponentFixture<ArtistComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ ArtistComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
+    });
+    //create instance of component, ArtistComponent
     fixture = TestBed.createComponent(ArtistComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
+   /**   .compileComponents();
+  }));
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+ beforeEach(() => {
+    fixture = TestBed.createComponent(ArtistComponent);
+   component = fixture.componentInstance;
+   fixture.detectChanges();
+  });
+    */
+   //select the target component and the toBeDefined method to do the test
+ //it("artist is defined", () => {expect(component).toBeDefined()});
+
 });
+
+// To-do: testing data bindings
+
